4220213da736d61ef40015c89ba049ce4e727471
Zaryab <zaryabkhan248@gmail.com>
2024-07-02 21:46:35 +0500

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "https://keep-notes-321t.onrender.com",
-    },
-  },
   plugins: [react()],
 });
